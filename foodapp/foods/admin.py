e938cac9ec0deb3cf2fc1ae9d0cc7d6d7d9f31ad
thansih-ahammed <thansihahammed7356@gmail.com>
2023-06-18 13:41:10 +0530

    create database models

--- foodapp/foods/admin.py
+++ foodapp/foods/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from foods.models import Food
 
-# Register your models here.
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "publisher_name"]
+
+admin.site.register(Food,FoodAdmin)
+
+
+
+
+
--- foodapp/foods/models.py
+++ foodapp/foods/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    publisher_name = models.CharField(max_length=200)
+    featured_image = models.ImageField(upload_to="foods/images")
+    ingredients = models.TextField()
+    description = models.TextField()
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'foods_food'
+
+    def __str__(self):
+        return self.name
+        
